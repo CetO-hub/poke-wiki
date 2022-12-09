d68633d1010bb37c799ec3dde30dfe1719c299d1
CetO-hub <skynet@SkyNet.localdomain>
2022-12-09 15:37:30 +0100

    Add 3 pokemon objects to the pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: [grass, poison],
+  },
+  {
+    name: "Charmander",
+    height: 0.6,
+    types: [fire],
+  },
+  {
+    name: "Jigglypuff",
+    height: 0.5,
+    types: [fairy, normal],
+  },
+];
