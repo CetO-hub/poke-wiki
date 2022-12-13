318c537542773737c2ce36f862b990cb1997ff6e
CetO-hub <skynet@SkyNet.localdomain>
2022-12-13 13:11:46 +0100

    feat: Add IIFE to the pokemonList and an add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["grass", "poison"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: ["fire"],
-  },
-  {
-    name: "Jigglypuff",
-    height: 0.5,
-    types: ["fairy", "normal"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: ["fire"],
+    },
+    {
+      name: "Jigglypuff",
+      height: 0.5,
+      types: ["fairy", "normal"],
+    },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
 
 // Iterate the pokemonList array and display the name and height of each item
 
-pokemonList.forEach(function (item) {
+pokemonRepository.getAll().forEach(function (item) {
   if (item.height > 0.6) {
     document.write(
       `<p class="pokemon-item">${item.name} (height: ${item.height} m) - Wow, that’s big! </p>`
